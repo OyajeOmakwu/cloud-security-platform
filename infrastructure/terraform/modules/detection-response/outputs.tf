6e4df276c55737e1dee2fecfa8ee68fef1b4b763
Oyaje Omakwu <oyajeomakwu2@gmail.com>
2026-03-25 17:09:00 -0400

    Update outputs.tf

--- infrastructure/terraform/modules/detection-response/outputs.tf
+++ infrastructure/terraform/modules/detection-response/outputs.tf
@@ -1,1 +1,11 @@
+output "sns_topic_arn" {
+  value = aws_sns_topic.security_alerts.arn
+}
 
+output "event_rule_name" {
+  value = aws_cloudwatch_event_rule.guardduty_high.name
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.remediation.function_name
+}
