88db8e9c10c53f3605ad7bdede760f25817b0fd5
Oyaje Omakwu <oyajeomakwu2@gmail.com>
2026-03-25 16:03:46 -0400

    Update variables.tf

--- infrastructure/terraform/modules/logging-monitoring/variables.tf
+++ infrastructure/terraform/modules/logging-monitoring/variables.tf
@@ -1,9 +1,15 @@
 variable "name" {
-  type        = string
   description = "Name prefix"
+  type        = string
+}
+
+variable "cloudtrail_bucket_name" {
+  description = "Globally unique S3 bucket name for CloudTrail logs"
+  type        = string
 }
 
 variable "tags" {
+  description = "Common tags"
   type        = map(string)
   default     = {}
 }
