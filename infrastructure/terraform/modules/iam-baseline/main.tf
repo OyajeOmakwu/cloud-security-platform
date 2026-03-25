36c8a840a73debc43153ef2b5d5e49f5d295be2d
Oyaje Omakwu <oyajeomakwu2@gmail.com>
2026-03-25 15:56:24 -0400

    Update main.tf

--- infrastructure/terraform/modules/iam-baseline/main.tf
+++ infrastructure/terraform/modules/iam-baseline/main.tf
@@ -1,31 +1,3 @@
-locals {
-  common_tags = merge(
-    {
-      Module = "iam-baseline"
-    },
-    var.tags
-  )
-}
-
-resource "aws_iam_role" "security_audit" {
-  name = "${var.name}-security-audit-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          AWS = "*"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-  tags = local.common_tags
-}
-
 resource "aws_iam_policy" "security_audit" {
   name        = "${var.name}-security-audit-policy"
   description = "Read-only security audit permissions"
@@ -34,14 +6,14 @@ resource "aws_iam_policy" "security_audit" {
     Version = "2012-10-17"
     Statement = [
       {
+        Sid    = "SecurityAuditReadOnly"
         Effect = "Allow"
         Action = [
           "ec2:Describe*",
-          "s3:Get*",
-          "s3:List*",
           "iam:Get*",
           "iam:List*",
-          "cloudtrail:Describe*",
+          "s3:Get*",
+          "s3:List*",
           "cloudtrail:Get*",
           "cloudtrail:List*",
           "guardduty:Get*",
@@ -53,9 +25,4 @@ resource "aws_iam_policy" "security_audit" {
       }
     ]
   })
-}
-
-resource "aws_iam_role_policy_attachment" "security_audit" {
-  role       = aws_iam_role.security_audit.name
-  policy_arn = aws_iam_policy.security_audit.arn
 }
