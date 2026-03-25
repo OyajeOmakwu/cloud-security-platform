f7a56c35803f54768827c0317c21d4f9e6c5cc91
Oyaje Omakwu <oyajeomakwu2@gmail.com>
2026-03-25 16:05:29 -0400

    Update outputs.tf

--- infrastructure/terraform/modules/logging-monitoring/outputs.tf
+++ infrastructure/terraform/modules/logging-monitoring/outputs.tf
@@ -1,7 +1,14 @@
 output "guardduty_detector_id" {
-  value = aws_guardduty_detector.this.id
+  description = "GuardDuty detector ID"
+  value       = aws_guardduty_detector.this.id
 }
 
 output "cloudtrail_name" {
-  value = aws_cloudtrail.this.name
+  description = "CloudTrail trail name"
+  value       = aws_cloudtrail.this.name
+}
+
+output "cloudtrail_bucket_name" {
+  description = "S3 bucket name for CloudTrail logs"
+  value       = aws_s3_bucket.cloudtrail.bucket
 }
